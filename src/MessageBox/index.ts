dd9d22b1b6f0756d4025fceaca53f1b576762588
liushengkai <liushengkai95@gm,ail.com>
2024-03-18 20:39:43 +0800

    feat： 指令式调用

---
+++ src/MessageBox/index.ts
@@ -0,0 +1,84 @@
+import { createApp, defineComponent, h } from 'vue'
+import Dialog from './Dialog.vue'
+
+interface MessageBox {
+  title?: string
+  content?: string
+  closeBtnText?: string
+  confirmBtnText?: string
+}
+
+function MessageBox() {
+  return {
+    alert(props: MessageBox) {
+      return new Promise((resolve) => {
+        const container = document.createElement('div')
+        const messagenBox = createApp(Dialog, {
+          ...props,
+          modelValue: true,
+          closeFn: onClose,
+          isDeclarative: true,
+          isShowCloseBtn: false
+        })
+        messagenBox.mount(container)
+        document.body.appendChild(container)
+        function onClose() {
+          resolve(undefined)
+          messagenBox.unmount()
+          document.body.removeChild(container)
+        }
+      })
+    },
+    confirm(props: MessageBox) {
+      return new Promise((resolve, reject) => {
+        const container = document.createElement('div')
+        const messagenBox = createApp(Dialog, {
+          ...props,
+          modelValue: true,
+          closeFn: onClose,
+          confirmFn: onConfirm,
+          isDeclarative: true
+        })
+        messagenBox.mount(container)
+        document.body.appendChild(container)
+        function onClose() {
+          reject(undefined)
+          messagenBox.unmount()
+          document.body.removeChild(container)
+        }
+        function onConfirm() {
+          resolve(undefined)
+          messagenBox.unmount()
+          document.body.removeChild(container)
+        }
+      })
+    }
+  }
+}
+
+const useMessageBox = MessageBox()
+const SkMessageBox = defineComponent({
+  props: {
+    modelValue: Boolean,
+    title: String,
+    content: String,
+    closeBtnText: String,
+    confirmBtnText: String,
+    isShowConfirmBtn: {
+      type: Boolean,
+      default: () => true
+    },
+    isShowCloseBtn: {
+      type: Boolean,
+      default: () => true
+    }
+  },
+  render() {
+    return h(Dialog, {
+      ...this.$props,
+      'onUpdate:modelValue': (value) => this.$emit('update:modelValue', value)
+    })
+  }
+})
+
+export { useMessageBox, SkMessageBox }
