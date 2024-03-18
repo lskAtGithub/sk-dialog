88822ab8c929904db5e8cca5558832ffc19002c5
liushengkai <liushengkai95@gm,ail.com>
2024-03-18 23:53:30 +0800

    feat： 打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { name as packageName } from './package.json'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      name: packageName,
+      entry: './src/MessageBox/',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `${packageName}.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
